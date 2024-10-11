0879ce9861b95829ee96182d39cce1a34e233455
tomaszm <tmoroz@orfix.pl>
2024-10-11 11:30:24 +0200

    router: routes/ index added

---
+++ src/router/routes.js
@@ -0,0 +1,23 @@
+import UserList from "@/components/UserList.vue";
+import AddUser from "@/components/AddUser.vue";
+import EditUser from "@/components/EditUser.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "UserList",
+    component: UserList,
+  },
+  {
+    path: "/add-user",
+    name: "AddUser",
+    component: AddUser,
+  },
+  {
+    path: "/edit-user/:id",
+    name: "EditUser",
+    component: EditUser,
+  },
+];
+
+export default routes
