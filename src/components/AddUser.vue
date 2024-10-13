3d44053f21843aa2ad22a3a9c770fd491ed3894f
tomaszm <tmoroz@orfix.pl>
2024-10-13 13:01:11 +0200

    components: AddUser/ EditUser added

---
+++ src/components/AddUser.vue
@@ -0,0 +1,8 @@
+<template>
+  <BaseUser action="add" />
+</template>
+
+<script setup>
+import BaseUser from './BaseUser.vue'
+</script>
+
---
+++ src/components/EditUser.vue
@@ -0,0 +1,8 @@
+<template>
+  <BaseUser action="edit" />
+</template>
+
+<script setup>
+import BaseUser from './BaseUser.vue'
+</script>
+
